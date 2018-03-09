9b430a539e43a504cb802611455e71e0914e7a01
taneresme <tnr.esme@gmail.com>
2018-03-09 13:24:54 +0300

    added ui project with thymeleaf

---
+++ wolower-api/src/main/java/com/wolower/mastercard/Masterpass.java
@@ -0,0 +1,15 @@
+package com.wolower.mastercard;
+
+import com.mastercard.sdk.core.util.QueryParams;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Masterpass {
+    public void obtainPairId(){
+//        QueryParams queryParams = new QueryParams()
+//                .add("pairingTransactionId", <pairingTransactionIdValue>)
+//                .add("userId", <userIdValue>);
+//
+//        Pairing pairingToken = PairingIdApi.show(queryParams);
+    }
+}
--- wolower-ui/src/main/java/com/wolower/WolowerUiApplication.java
+++ wolower-ui/src/main/java/com/wolower/WolowerUiApplication.java
@@ -1,4 +1,4 @@
-package com.wolower.ui;
+package com.wolower;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
