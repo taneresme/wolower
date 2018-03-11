51f2935e1b46c45011f3244411134b79a503349c
taneresme <tnr.esme@gmail.com>
2018-03-11 09:41:17 +0200

    Masterpass
    thymeleaf tests

---
+++ wolower-ui/src/main/java/com/wolower/ui/WolowerUiApplication.java
@@ -0,0 +1,13 @@
+package com.wolower.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolowerUiApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(WolowerUiApplication.class, args);
+    }
+	
+}
