08d301ef80895c3a44efa56d40a4d7a662b116ba
taneresme <tnr.esme@gmail.com>
2018-05-01 14:45:29 +0300

    modifications after codacy review

--- wolower-ui/src/main/resources/static/js/custom.js
+++ wolower-ui/src/main/resources/static/js/custom.js
@@ -1,18 +1,11 @@
-String.prototype.format = function(args) {
-	var str = this;
-	return str.replace(String.prototype.format.regex, function(item) {
-		var intVal = parseInt(item.substring(1, item.length - 1));
-		var replace;
-		if (intVal >= 0) {
-			replace = args[intVal];
-		} else if (intVal === -1) {
-			replace = "{";
-		} else if (intVal === -2) {
-			replace = "}";
-		} else {
-			replace = "";
-		}
-		return replace;
-	});
-};
-String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number] 
+        : match
+      ;
+    });
+  };
+}
