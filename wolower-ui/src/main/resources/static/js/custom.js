65dba71d3584f1efaee39c2637640aa91c3d2012
taneresme <tnr.esme@gmail.com>
2018-05-01 15:22:44 +0300

    after codacy review

--- wolower-ui/src/main/resources/static/js/custom.js
+++ wolower-ui/src/main/resources/static/js/custom.js
@@ -2,8 +2,8 @@ if (!String.format) {
   String.format = function(format) {
     var args = Array.prototype.slice.call(arguments, 1);
     return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != "undefined"
-        ? args[number] 
+      return typeof $(args).get(number) != "undefined"
+        ? $(args).get(number)
         : match
       ;
     });
