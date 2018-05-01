b52e77dedad7bc41e03f50843ad0dd480e488c91
taneresme <tnr.esme@gmail.com>
2018-05-01 15:07:21 +0300

    [Solved] Strings must use double quote. (quotes)

--- wolower-ui/src/main/resources/static/js/custom.js
+++ wolower-ui/src/main/resources/static/js/custom.js
@@ -2,7 +2,7 @@ if (!String.format) {
   String.format = function(format) {
     var args = Array.prototype.slice.call(arguments, 1);
     return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
+      return typeof args[number] != "undefined"
         ? args[number] 
         : match
       ;
