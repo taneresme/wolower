4eef5af9e9508ba719abdc859303446981eb63ef
taneresme <tnr.esme@gmail.com>
2018-05-06 17:50:17 +0300

    code quality improved

--- wolower-ui/src/main/java/com/wolower/ui/controller/WhatWeDoController.java
+++ wolower-ui/src/main/java/com/wolower/ui/controller/WhatWeDoController.java
@@ -1,8 +1,6 @@
 package com.wolower.ui.controller;
 
 import com.wolower.ui.service.HeaderService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WhatWeDoController {
-	private static final Logger LOGGER = LoggerFactory.getLogger(MasterpassController.class);
-
 	@Autowired
 	private HeaderService header;
 
